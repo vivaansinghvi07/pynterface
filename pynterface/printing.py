e2d8a5d255364d3ca0038b898573110869c9b732
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-05-08 22:34:14 -0400

    added clearing and printing with clearing

--- pynterface/printing.py
+++ pynterface/printing.py
@@ -1,10 +1,26 @@
 """
 TODO:
 - Smooth print
-- Clear then print
-- Clear screen
 - Rainbow text
 - Align to center
 - Hide cursor, show cursor
 """
 
+import os
+from typing import Any
+
+def clear_window() -> None:
+    """
+    Clears the terminal window.
+    """
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def clear_print(message: Any, end: str = "\n") -> None:
+    """
+    Clears the terminal window and prints the prompt.
+    """
+    clear_window()
+    print(message, end=end)

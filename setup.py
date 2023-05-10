839c05d23fa31961cd6ff3526edb668f1ba639f9
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-05-09 22:34:44 -0400

    remove need for requirements

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     description = f.read()
 
-# with open("requirements.txt") as f:
-#     requirements = f.readlines()
-
 setup(
     name='pynterface',
     version='1.0.0',
@@ -20,6 +17,5 @@ setup(
     include_package_data=True,
     project_urls={
         "Documentation": "https://unisci.readthedocs.io/en/latest/"
-    },
-    # install_requires=requirements
+    }
 )
